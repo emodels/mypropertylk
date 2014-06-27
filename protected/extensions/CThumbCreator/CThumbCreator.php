f4d1a37e8dc7892cb58f1752d25cd1a43f0ea447
danesh <emodels#gmail.com>
2014-06-28 01:32:40 +0530

    Thumb creator hack

--- protected/extensions/CThumbCreator/CThumbCreator.php
+++ protected/extensions/CThumbCreator/CThumbCreator.php
@@ -171,7 +171,7 @@
                     $this->img = imagecreatefrompng($this->image);
                     break;
                 default:
-                    throw new Exception("Tipo de imagem nao suportado",500);
+                    //throw new Exception("Tipo de imagem nao suportado",500);
             }
         }
 
@@ -186,6 +186,11 @@
             if(!$this->tmp)
             {
                 $this->createImg();
+
+                if (!$this->img) { //-------Added by Danesh
+                    return;
+                }
+
             } else {
                 $this->img = $this->tmp;
             }
