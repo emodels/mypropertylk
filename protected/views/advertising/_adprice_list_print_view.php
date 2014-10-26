3e587c2231c4c61c5194c4524e04dc725af8a8fc
danesh <emodels#gmail.com>
2014-10-26 16:16:01 +0530

    Price list "per week" addition

--- protected/views/advertising/_adprice_list_print_view.php
+++ protected/views/advertising/_adprice_list_print_view.php
@@ -4,7 +4,7 @@
             <div class="span3" style="font-size: 22px; font-weight: bold">
                 <div><?php echo  $data->page0->page ; ?></div>
                 <div class="listing-normal" style="margin-top: 20px"><?php echo  "Ad Size: " . $data->size0->size ; ?></div>
-                <div class="listing-normal"><?php echo "Price: Rs. " . Yii::app()->numberFormatter->format("#,##0.00", $data->price) ; ?></div>
+                <div class="listing-normal"><?php echo "Price: Rs. " . Yii::app()->numberFormatter->format("#,##0.00", $data->price) ; ?><span style="color: red; padding-left: 10px">(Per week)</span></div>
             </div>
             <div class="span5" style="max-width: 445px;">
                 <img src="<?php echo Yii::app()->request->baseUrl; ?>/upload/adsampleimages/<?php echo $data->adsample; ?>" style="border: solid 1px silver"/>
