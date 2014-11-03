1a129e4684a88c4330226c6acc9eb7301720a90e
danesh <emodels#gmail.com>
2014-11-03 15:46:56 +0530

    Facebook meta tags added

--- protected/controllers/List/DetailAction.php
+++ protected/controllers/List/DetailAction.php
@@ -32,10 +32,10 @@ class DetailAction extends CAction
                 Yii::app()->clientScript->registerMetaTag($model->desc, 'description');
 
                 /*---( Facebook specific Meta tags )---*/
-                Yii::app()->clientScript->registerMetaTag($model->desc, 'og:description');
+                Yii::app()->clientScript->registerMetaTag($model->desc, null, null, array('property'=>'og:description'));
 
                 if (count($model->propertyimages) > 0) {
-                    Yii::app()->clientScript->registerMetaTag('http://www.myproperty.lk/upload/propertyimages/'. $model->propertyimages[0]->imagename, 'og:image');
+                    Yii::app()->clientScript->registerMetaTag('http://www.myproperty.lk/upload/propertyimages/'. $model->propertyimages[0]->imagename, null, null, array('property'=>'og:image'));
                 }
 
                 if ($model->balcony == 1) {
